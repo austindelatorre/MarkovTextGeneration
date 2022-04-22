1f7daf81c14a551d02c4078682a87a322e942392
Cole <cadumas01@gmail.com>
2022-04-21 22:48:46 -0400

    trouble parsing some urls still

--- src/feed_carl.py
+++ src/feed_carl.py
@@ -5,7 +5,7 @@ import to_json as json
 #Loops through the links in "urls" txt file and writes their prob_dicts to our json file
 
 def feed():
-    with open("urls.txt", "r") as file:
+    with open("carl_urls.txt", "r") as file:
         text = []
         for line in file:
             text += parse.get_text(line)
