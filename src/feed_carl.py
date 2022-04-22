297b77071ce67f66e8ad7d7f48193cb4fb39c7e3
colimartin <colin.martin.01@gmail.com>
2022-04-21 20:55:15 -0400

    fixed 2D list issue

--- src/feed_carl.py
+++ src/feed_carl.py
@@ -2,13 +2,12 @@ import parse
 import list_to_markov as markov
 import to_json as json
 
-#Loops through the links in "carl_urls" txt file and writes their prob_dicts to our json file
+#Loops through the links in "urls" txt file and writes their prob_dicts to our json file
 
 def feed():
     with open("urls.txt", "r") as file:
-        text =
+        text = []
         for line in file:
-            text.append(parse.get_text(line))
+            text += parse.get_text(line)
            #  json.write(markov.list_to_markov(parse.get_text(line), 2))
-        print(text)
-        json.write(markov.list_to_markov(text, 2))
+        json.write(markov.list_to_markov(text, 2), "dict")
