1450e73103efdf87f32a7d06b2c37d12252f4d11
colimartin <colin.martin.01@gmail.com>
2022-04-25 21:04:43 -0400

    fixed url looping

--- backend/feed_carl.py
+++ backend/feed_carl.py
@@ -5,11 +5,11 @@ import to_json as json
 #Loops through the links in "urls" txt file and writes their prob_dicts to our json file
 
 def feed():
-    with open("carl_urls.txt", "r") as file:
+    with open("urls.txt", "r") as file:
         text = []
         for line in file:
-            text += parse.get_text(line)
-            print("Just parsed ", line )
+            line = line.rstrip('\r\n')
+            new_text = parse.get_text(line)
+            text += new_text
            #  json.write(markov.list_to_markov(parse.get_text(line), 2))
-        print(text)
         json.write(markov.list_to_markov(text, 5), "dict")
