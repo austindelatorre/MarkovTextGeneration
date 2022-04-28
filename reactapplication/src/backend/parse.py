f7d54e28bd8e5d06f4e5f955699e440544563328
Dylan Leddy <leddyd@bc.edu>
2022-04-28 12:51:54 -0400

    parse v.2 --> accounts for apostrophes, dashes and periods that don't appear at the end of sentences. Added inline comments

--- reactapplication/src/backend/parse.py
+++ reactapplication/src/backend/parse.py
@@ -3,13 +3,8 @@ import requests
 import re
 
 
-# Notes for improvement:
-# - Spacing / readability
-# - Some words do not have account for spaces properly (which means some keys are jammed together)
-# - Watch for apostrophes
-
 # Takes a url and returns the webpage's tokenized text using beautifulsoup.
-# Cleans the text using regexp (removes numbers, equations, punctuation, and lowercases every string).
+# Cleans the text using regexp (removes numbers, equations, special symbols, and lowercases every string).
 # -- I'm sure the data cleaning can be improved
 # Does not comb through multiple pages yet.
 def get_text(url):
@@ -19,17 +14,16 @@ def get_text(url):
     soup = BeautifulSoup(html, 'html.parser')
     paragraphs = soup.findAll('p')
     for paragraph in paragraphs:
-        clean_string = re.sub(r'(\$+)(?:(?!\1)[\s\S])*\1', '', paragraph.text)
-        clean_string = re.sub('[^A-Za-z0-9. ]+', '', clean_string)
-        clean_string = re.sub(r'\w*\d\w*', '', clean_string).strip()
-        clean_string = clean_string.replace("i.e.", "")
-        clean_string = clean_string.lower()
-        sentence_list = [sentence for sentence in clean_string.split('.')]
+        clean_string = re.sub(r"(\$+)(?:(?!\1)[\s\S])*\1|[^A-Za-z0-9 .-]+", '', paragraph.text)  # special characters
+        # clean_string = re.sub(r'\w*\d\w*', '', clean_string).strip() -- numbers
+        clean_string = clean_string.replace("i.e.", "")  # removes i.e.
+        clean_string = clean_string.lower()  # lowercases string
+        sentence_list = [sentence for sentence in clean_string.split('. ')]  # splits into list of sentences
         for element in sentence_list:
             text_matrix.append(element)
     for sentence in text_matrix:
-        words = sentence.split()
-        output_matrix.append(words)
+        words = sentence.split()  # splits each sentence into a list of words
+        output_matrix.append(words)  # appends to one big master list
     for element in output_matrix:
         if len(element) < 2:
             output_matrix.remove(element)
