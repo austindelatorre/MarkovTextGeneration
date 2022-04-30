c70b7fc991cfc8cdb6c3c4c47573f3bdac27cb1e
austindelatorre <austindlt@gmail.com>
2022-04-30 14:46:02 -0400

    feed carl changes

--- reactapplication/src/backend/feed_carl.py
+++ reactapplication/src/backend/feed_carl.py
@@ -2,6 +2,7 @@ import parse
 import strict_parse
 import list_to_markov as markov
 import to_json as json
+import os
 
 #Loops through the links in "urls" txt file and writes their prob_dicts to our json file
 
@@ -18,6 +19,8 @@ def feed():
         json.write(markov.list_to_markov(text, 3), "dict")
 
         print("CARL IS FED")
+        file_size = os.path.getsize('dict.json')
+        print("File Size of dict.json is :", file_size/1000000, "mb")
 
 if __name__ == "__main__":
     feed()
