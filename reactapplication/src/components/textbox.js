234003caae7237086436d6f37456d8072795d919
MattMurn0 <murno@bc.edu>
2022-04-30 12:50:02 -0400

    almost working suggestion box

--- reactapplication/src/components/textbox.js
+++ reactapplication/src/components/textbox.js
@@ -2,15 +2,21 @@ import React, {useState} from 'react';
 import {suggest_text} from "../backend/suggest_text.js"
 import css from "./textbox.module.css"
 
+//storing the previous input to subtract from input so search only using the current word
 var lastWord = null;
+
+//output of suggest text
 var suggestions;
 //var trimmedInput = null;
 
 export default function Textbox(props) 
 {
+    //importing the json file
     const {data} = props;
-  
+    
     const [input, setInput] = useState('');
+    const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+    const [showSuggestions, setShowSuggestions] = useState(false);
 
     function findWords(a) {
         if (lastWord === null)
@@ -34,17 +40,54 @@ export default function Textbox(props)
     console.log("INPUT VALUE", input);
     if (input.endsWith(' '))
     {
-        console.log("THIS IS WERE WE AUTO SUGGEST THE PREVIOUS VALUE")
-        console.log("INPUT WITHOUT THE SPACE?", input.trim());
+        //console.log("THIS IS WERE WE AUTO SUGGEST THE PREVIOUS VALUE")
+        //console.log("INPUT WITHOUT THE SPACE?", input.trim());
         findWords(input);
         console.log("suggesions", suggestions);
+    }
+
+    const onChange = (e) => {
+        setInput(e.target.value);
+        setActiveSuggestionIndex(0);
+        setShowSuggestions(true);
+      };
+    
+
+    const onClick = (e) => {
+        setInput(e.target.innerText);
+        setActiveSuggestionIndex(0);
+        setShowSuggestions(false);
+      };
+
+    const AutoSuggestionList = () => {
+        return suggestions.length ? (
+          <ul class="suggestions">
+            {suggestions.map((suggestion, index) => {
+              let className;
+              // Flag the active suggestion with a class
+              if (index === activeSuggestionIndex) {
+                className = "suggestion-active";
+              }
+              return (
+                <li className={className} key={suggestion} onClick={onClick}>
+                  {suggestion}
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div class="no-suggestions">
+            <em>Type a word and press space first!</em>
+          </div>
+        );
     }
 
     return(
         <div className = {css.content}>
             <form autoComplete = "off">
                 <div className = {css.autocomplete}>
-                    <textarea type="text" placeholder="Type a word" id="carlInput" value={input} onChange={e=>setInput(e.target.value)}/>
+                    <textarea type="text" placeholder="Type a word and press space for a suggestion" id="carlInput" value={input} onChange={onChange} onClick={onClick}/>
+                    {showSuggestions && lastWord && <AutoSuggestionList />}
                 </div>
             </form>
         </div>
